3c37fc7368e3d12da45a833f5dda8bda4a900514
Pavel Isupov <exride@gmail.com>
2019-04-30 17:29:07 +1200

    frontend nearly finished

--- src/components/CategoryManagementComponents/DetailsCategoryManagement.vue
+++ src/components/CategoryManagementComponents/DetailsCategoryManagement.vue
@@ -1,6 +1,6 @@
 <template>
-    <b-container fluid class="">
-        <b-col class="whiteWrapper borders">
+    <b-container fluid class="no-intent whiteWrapper">
+        <b-col class="no-intent borders">
             <p class="details">Select an item to see complete details</p>
         </b-col>
     </b-container>
@@ -13,6 +13,21 @@ export default {
 </script>
 
 <style scoped>
+    .whiteWrapper {
+        height: 100%;
+        background-color: #ffffff;
+    }
+
+    .no-intent {
+        padding: 0;
+    }
+    
+    .borders {
+        border-style: solid;
+        border-color: #9c9b9b;
+        border-width: 1px;
+    }
+
     .details {
         margin-right: 15px;
         height: 300px;
@@ -20,9 +35,5 @@ export default {
         font-size: auto;
         color:  #283593;
         font-size: 12px;
-    }
-
-    .details-gap {
-        margin: 15px;
     }
 </style>
--- src/components/CategoryManagementComponents/MainCategoryManagement.vue
+++ src/components/CategoryManagementComponents/MainCategoryManagement.vue
@@ -1,9 +1,17 @@
 <template>
-    <b-container fluid class="">
-        <b-col class="whiteWrapper borders auto-height no-gaps">
-            <h2 class="headline">Main Categories</h2>
-            <hr>
-        </b-col>
+    <b-container fluid class="borders whiteWrapper">
+        <b-row class="auto-height">
+            <b-col class="no-intent">
+                <h2 class="headline">Main Categories</h2>
+                <hr>
+            </b-col>
+        </b-row>
+        <b-row>
+            Existing Main Categories here
+        </b-row>
+        <b-row>
+            <b-button class="button appPrimaryBackgroundColor" v-on:click="ItemNameSearch(itemName)">Add New Category</b-button>
+        </b-row>
     </b-container>
 </template>
 
@@ -14,5 +22,39 @@ export default {
 </script>
 
 <style scoped>
+    .whiteWrapper {
+        height: 100%;
+        background-color: #ffffff;
+    }
+
+    .no-intent {
+        padding: 0;
+    }
+
+    .headline {
+        color:  #283593;
+        padding: 15px 0px 0px 15px;
+        font-weight: bold;
+        font-size: 24px;
+    }
+
+    .borders {
+        border-style: solid;
+        border-color: #9c9b9b;
+        border-width: 1px;
+    }
+
+    .button {
+        margin: 15px 0px 15px 15px;
+        font-size: 14px;  
+    }
     
+    .auto-height {
+        height: auto;
+    }
+    
+    hr {
+        border: 0.5px solid #283593;
+        margin: 5px 0px;
+    }
 </style>
