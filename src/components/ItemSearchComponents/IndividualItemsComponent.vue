5a1c71a40ff9abf55e80a05e923f568466a9f975
Ercel Peter Sotto <ercelpetersotto@gmail.com>
2019-05-16 11:06:07 +1200

    Change the reference for Item Number in View Item Detail to a variable declared within the component. This variable will contain the Item Number instead of the one passed by the parent component to bypass the trigger for View Item Detail data refresh.

--- src/components/ItemSearchComponents/IndividualItemsComponent.vue
+++ src/components/ItemSearchComponents/IndividualItemsComponent.vue
@@ -4,18 +4,18 @@
             <b-container fluid>
                 <b-row>
                     <b-col lg="9" md="9" sm="9">
-                        <h1 v-if="itemNumber == 0">
+                        <h1 v-if="itemId == 0">
                             Item Number
                         </h1>
-                        <h1 v-if="itemNumber != 0">
-                            {{itemNumber}}
+                        <h1 v-if="itemId != 0">
+                            {{itemId}}
                         </h1>
                     </b-col>
                     <b-col lg="3" md="3" sm="3">
                         <font-awesome-icon class="icons appPrimaryTextColor" icon="print" v-on:click="printForm"/>
-                        <font-awesome-icon class="icons appPrimaryTextColor" icon="plus" v-on:click="addNewItem" v-if="itemNumber == 0 && !showCancelButton"/>
+                        <font-awesome-icon class="icons appPrimaryTextColor" icon="plus" v-on:click="addNewItem" v-if="itemId == 0 && !showCancelButton"/>
                         <font-awesome-icon class="icons appPrimaryTextColor" icon="times" v-on:click="onReset" v-if="showCancelButton"/>
-                        <font-awesome-icon class="icons appPrimaryTextColor" icon="edit" v-on:click="modifyItem" v-if="itemNumber != 0 && !showCancelButton"/>
+                        <font-awesome-icon class="icons appPrimaryTextColor" icon="edit" v-on:click="modifyItem" v-if="itemId != 0 && !showCancelButton"/>
                     </b-col>
                 </b-row>
                 <b-row>
@@ -98,7 +98,7 @@
                                                 Threshold (%)
                                             </b-col>
                                             <b-col lg="8" md="8" sm="8" class="inputColumn">
-                                                <b-input block id="measuredBy" name="measuredBy" v-model="form.thresholdQty" :readonly="readOnly" :required="true" size="sm"/>
+                                                <b-input block id="thresholdQty" name="thresholdQty" v-model="form.thresholdQty" :readonly="readOnly" :required="true" size="sm"/>
                                             </b-col>
                                         </b-row>
                                         <b-row class="generalInfoContainer">
@@ -227,6 +227,7 @@ export default {
             subCategoryList: [{value: null, text: "Select a SubCategory"}],
             statusList: [{value: null, text: "Select a Status"}],
             locationList: [{value: null, text: "Select a Location"}],
+            itemId: 0,
         };
     },
     methods: {
@@ -236,7 +237,7 @@ export default {
             if(!this.toModify){
                 axios.post("http://localhost:49995/api/ItemManagement/InsertNewItem", item)
                     .then(res => {
-                        console.log(res.data);
+                        this.itemId = res.data.Result.Id;
                         this.showCancelButton = false;
                     })
                     .catch(err => {
@@ -301,7 +302,6 @@ export default {
                     .then(res => {
                         if(res.data.Result.length != 0){
                             this.form.itemDetail = res.data.Result;
-                            console.log(res.data.Result);
                         } else {
                             this.form.itemDetail = [];
                         }
@@ -310,7 +310,7 @@ export default {
             }
         },
         triggerItemStatusChange() {
-            const status = {Id: this.itemNumber, StatusCd: this.form.isActive};
+            const status = {Id: this.ItemId, StatusCd: this.form.isActive};
 
             axios.post("http://localhost:49995/api/ItemManagement/UpdateItemStatusById", status)
                 .then(res => {
@@ -364,7 +364,7 @@ export default {
                                     var subCatItem = {
                                         value: res2.data[i].Id, text: res2.data[i].SubCategoryName
                                     }
-
+                                    this.itemId = this.itemNumber;
                                     this.subCategoryList = this.subCategoryList.concat(subCatItem);
                                     this.form.itemName = res.data.Result.ItemName;
                                     this.form.categoryId = res.data.Result.CategoryId;
@@ -403,6 +403,7 @@ export default {
                     tags: "",
                     itemDetail: [],
                 }
+                this.itemId = 0;
             }
         },
     }
