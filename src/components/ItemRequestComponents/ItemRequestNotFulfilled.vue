39b50668ce0f56945dabcd051169145ec6642328
Ercel Peter Sotto <ercelpetersotto@gmail.com>
2019-05-28 14:17:50 +1200

    -Completed adding pagination for delinquents table in Item Request page.

--- src/components/ItemRequestComponents/ItemRequestNotFulfilled.vue
+++ src/components/ItemRequestComponents/ItemRequestNotFulfilled.vue
@@ -6,31 +6,81 @@
             selectable
             select-mode="single"
             selectedVariant="success"
-            :items="itemRequestDelinquents" 
+            :items="itemRequestList" 
             show-empty
             @row-selected="rowSelected"
             :fields="fields"
-           >            
+            :current-page="currentPage"
+            :per-page="perPage">            
         </b-table> 
 
+         <b-pagination v-model="currentPage"
+            :total-rows="recordCount"
+            @change="getNextBatchSearchResult"
+            align="right"
+            v-if="showPagination"
+            size="md"
+            first-text="FIRST"
+            next-text="NEXT"
+            prev-text="PREVIOUS"
+            last-text="LAST"
+            :per-page="perPage">
+        </b-pagination>
       
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import moment from "moment";
+
 export default {
     name: 'ItemRequestNotFulfilled',
     props: ['itemRequestDelinquents'],
     data() {
         return {
             fields: [{key:'Id' ,label: 'ID'}, 'Title', 'Status', {key:'DateCreated', label:'Date Created'}, {key: 'TicketStatus', label:'Overall Status'}],
-            selected: []
+            selected: [],
+            itemRequestList: [],
+            currentPage: 1,
+            perPage: 10,
+            showPagination: false,
+            recordCount: 0
         }
     },
     methods: {
         rowSelected(items){
             this.selected = items;
             this.$emit('selected-item-delinquent', this.selected);
+        },
+        getNextBatchSearchResult(page){
+            if(this.itemRequestList.length < this.recordCount){
+                const currentPage = page;
+
+                axios.post("http://localhost:50006/api/PurchaseOrderManagement/GetItemRequestFormDelinquents", {currentPage})
+                    .then( res => {
+                        for(var i = 0; i < res.data.SearchResult.length; i++){
+                            let singleResult = res.data.SearchResult[i];
+                            singleResult.DateCreated = moment(singleResult.DateCreated).format("MMM DD, YYYY");
+                            this.itemRequestList = this.itemRequestList.concat(singleResult);
+                        }
+                    }). catch( err => {console.log(err);});
+            }
+        }
+    },
+    watch: {
+        itemRequestDelinquents: function(){
+            if(this.itemRequestDelinquents.RecordCount > this.perPage){
+                this.showPagination = true;
+            }
+
+            this.recordCount = this.itemRequestDelinquents.RecordCount;
+            this.itemRequestList = this.itemRequestDelinquents.SearchResult;
+            this.currentPage = 1;
+
+            for(var i = 0; i < this.itemRequestList.length; i++){
+                this.itemRequestList[i].DateCreated = moment(this.itemRequestList[i].DateCreated).format("MMM DD, YYYY");
+            }
         }
     }
 }
