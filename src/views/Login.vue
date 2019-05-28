4732452db5c4b4e6cfcb59db1a9c0eb4c4893790
Pavel Isupov <exride@gmail.com>
2019-05-29 00:12:56 +1200

    Login page design intermediate stage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,50 @@
 <template>
-<!-- <b-container>
-    <div id="login">
-        <h1>Login</h1>
-        <input type="text" name="username" v-model="input.username" placeholder="Username" />
-        <input type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
-    </div>
-</b-container> -->
 
-<b-container fluid class="login-container">
-    <h2>Login</h2>
-    <div class="row">
-        <div class="col-md-12 form-group">
+<b-container fluid class="login-container background">
+    <b-row>
+        <b-col>
+            <h1 class="header-title">Welcome to ORMOC LANTERN HARDWARE AND SUPPLIES</h1>
+        </b-col>
+    </b-row>
+    <b-row>
+        <b-col>
+            <h2 class="subheader-title">Login</h2>
+        </b-col>
+    </b-row>
+    <b-row align-h="center" class="input-group form-group">
+        <b-col class="col-md-6 input-group-prepend">
+                <span class="fa-align-custom">
+                    <font-awesome-icon class="icon fa-align-center" icon="user" />
+                </span>
             <input 
-                type="text"
-                class="form-control"
-                placeholder="username"
-                v-model="input.username">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12 form-group">
+            type="text"
+            class="form-control input-custom"
+            placeholder="Username"
+            v-model="input.username"
+            required>
+        </b-col>
+    </b-row>
+    <b-row align-h="center" class="input-group form-group">
+        <b-col class="col-md-6 input-group-prepend">
+            <span class="fa-align-custom">
+                <font-awesome-icon class="icon fa-align-center" icon="key" />
+            </span>
             <input 
-                type="password"
-                placeholder="password"
-                class="form-control"
-                v-model="input.password">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12 form-group">
-            <button 
-                type="button"
-                class="btn btn-block btn-login"
-                v-on:click="login()">Login
-            </button>
-        </div>
-    </div>
+            type="password"
+            placeholder="Password"
+            class="form-control input-custom"
+            v-model="input.password"
+            required>
+        </b-col>
+    </b-row>
+    <b-row align-h="center" class="input-group form-group">
+        <b-col class="col-md-3">
+            <b-button
+                @click="login()"
+                class="btn-block btn-login">Enter
+            </b-button>
+        </b-col>
+    </b-row>
 </b-container>
 
 </template>
@@ -44,7 +52,7 @@
 <script>
 
 export default {
-    name: 'login',  
+    name: 'login',
     data() {
         return {
             input: {
@@ -60,8 +68,9 @@ export default {
                     this.$emit("authenticated", true);
                     this.$router.replace({ name: "home" });
                 } else {
-                    // eslint-disable-next-line
-                    console.log("The username and / or password is incorrect");
+                    // Alert about wrong credentials and reset typed fields
+                    alert("The username and / or password is incorrect");
+                    this.input = [];
                 }
             } else {
                 // eslint-disable-next-line
@@ -73,27 +82,60 @@ export default {
 </script>
 
 <style scoped>
-    #login {
-        width: 500px;
-        border: 1px solid #CCCCCC;
-        background-color: #FFFFFF;
-        margin: auto;
-        margin-top: 200px;
-        padding: 20px;
-    }
-
-.login-container{
-    width: 300px;
-    max-width: 100%;
-    margin: 50px auto;
+.background {
+    background: #e8eaf6;
+    padding: 20px;
+    border-radius: 15px;
 }
-.login-container h2{
+
+.header-border {
+    border: 1px solid #283593;
+    margin: 5px 0px;
+}
+
+.header-title {
+    font-size: 24px;
     text-align: center;
-    font-size: 20px;
+    color: #fff;
+    background-color: #283593;
+    padding: 5px;
+    border-radius: 5px;
 }
 
-.login-container .btn-login{
+.subheader-title {
+    margin: 10px 0px 15px 0px;
+    color: #283593;
+    text-align: center;
+}
+
+.login-container {
+    width: 530px;
+    max-width: 100%;
+    margin: 150px auto;
+}
+
+.login-container .btn-login {
     background-color: #283593;
     color: #fff;
 }
+
+.input-group-prepend span {
+    width: 50px;
+    border-radius: 5px;
+    background-color: #283593;
+    color: #ffffff;
+    border:0 !important;
+}
+
+.input-custom {
+    margin-left: -6px;
+}
+
+.fa-align-custom {
+    display: inline-block; 
+    text-align: center;
+    font-size: 18px;
+    padding-right: 5px;
+}
+
 </style>
